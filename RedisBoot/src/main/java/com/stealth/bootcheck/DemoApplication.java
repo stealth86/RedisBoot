6118d365335766d5478b68c5cf63ec86ed3db176
stealth86 <sathyaprabu@gmail.com>
2017-03-04 11:56:53 -0500

    commit 1

--- RedisBoot/src/main/java/com/stealth/bootcheck/DemoApplication.java
+++ RedisBoot/src/main/java/com/stealth/bootcheck/DemoApplication.java
@@ -2,8 +2,10 @@ package com.stealth.bootcheck;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -13,8 +15,11 @@ import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 
+import com.stealth.model.XmlBean;
+
 @SpringBootApplication
 @ComponentScan(basePackages="com.stealth")
+@ImportResource("classpath:addconfig.xml")
 public class DemoApplication {
 
 	@Bean
@@ -47,6 +52,8 @@ public class DemoApplication {
 	}
 	
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		ConfigurableApplicationContext cap=SpringApplication.run(DemoApplication.class, args);
+		XmlBean xb=(XmlBean)cap.getBean(XmlBean.class);
+		System.out.println(xb.getXmlId()+" "+xb.getXmlName());
 	}
 }
---
+++ RedisBoot/src/main/java/com/stealth/model/XmlBean.java
@@ -0,0 +1,21 @@
+package com.stealth.model;
+
+public class XmlBean {
+	private int xmlId;
+	private String xmlName;
+	public XmlBean() {
+		// TODO Auto-generated constructor stub
+	}
+	public String getXmlName() {
+		return xmlName;
+	}
+	public void setXmlName(String xmlName) {
+		this.xmlName = xmlName;
+	}
+	public int getXmlId() {
+		return xmlId;
+	}
+	public void setXmlId(int xmlId) {
+		this.xmlId = xmlId;
+	}
+}
